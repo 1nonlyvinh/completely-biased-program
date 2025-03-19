dc2da028fdb85390ce7a8bdc180b1b4c8477aaa3
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2025-03-19 12:30:26 +0000

    help

--- bias.py
+++ bias.py
@@ -1,5 +1,6 @@
 import pdfminer
 print(pdfminer.__version__)  
+points = 0
 
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
@@ -15,18 +16,27 @@ def extract_text_from_pdf(pdf_path):
 
 pdf_text = extract_text_from_pdf('fake-resume.pdf')
 
-def find_keywords(text, keywords):
-    found_keywords = []
+def find_ed_keywords(text, keywords):
+    found_ed_keywords = []
     for keyword in keywords:
         if keyword.lower() in text.lower():
-            found_keywords.append(keyword)
-    return found_keywords
+            found_ed_keywords.append(keyword)
+    return found_ed_keywords
 
-keywords_to_search = ['the', 'scoobydoo', 'hi', 'accomplishments']
-found_keywords = find_keywords(pdf_text, keywords_to_search)
+keywords_to_search = ['university', 'degree', 'college', 'major', 'minor', 'trade school', 'high school']
+found_keywords = find_ed_keywords(pdf_text, keywords_to_search)
 
 
 if found_keywords:
     print("Keywords found:", found_keywords)
 else:
     print("No keywords found.")
+
+def rank_ed(): 
+    if 'degree' in found_keywords or 'college'  in found_keywords:
+        global points
+        points += 2
+
+rank_ed()
+
+print(f'Points: {points}')
