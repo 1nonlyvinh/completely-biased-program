3f27b2fdb38862d7ebfa825c8ae623eb4801d37e
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2025-03-28 15:27:12 +0000

    finishin = up

--- bias.py
+++ bias.py
@@ -266,7 +266,6 @@ if skills_found_keywords2:
 else:
     print("No keywords found.")
 
- 
  # prints points Fuck you vincentFuck you vincentFuck you vincentFuck you vincent
 print(f'Points for Resume 2: {points2}')
 
@@ -401,3 +400,402 @@ else:
  
  # prints points Fuck you vincentFuck you vincentFuck you vincentFuck you vincent
 print(f'Points for Resume 3: {points3}')
+
+
+points4 = 0 
+
+with open('resume4.pdf', 'rb') as fin:
+    extract_text_to_fp(fin, output_string)
+print(output_string.getvalue().strip())
+ 
+pdf_text = extract_text('resume4.pdf')
+pdf_path = extract_text('resume4.pdf')
+
+def extract_text_from_pdf4(pdf_path):
+    return extract_text(pdf_path)
+
+# finds keywords
+def find_ed_keywords4(text, keywords):
+    found_ed_keywords4 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_ed_keywords.append(keyword)
+    return found_ed_keywords
+
+# keywords to find
+ed_keywords_to_search3 = ['university', 'degree', 'college', 'major', 'minor', 'trade school', 'high school']
+ed_found_keywords3 = find_ed_keywords(pdf_text, ed_keywords_to_search)
+
+ # message to say if keywords are found
+if ed_found_keywords3:
+    print("Keywords found:", ed_found_keywords3)
+else:
+    print("No keywords found.")
+ 
+ # rank keywords related to education 
+def rank_ed4():
+    if 'university' in ed_found_keywords:
+        global points4
+        points4 += 2
+    if 'degree' in ed_found_keywords:
+        points4 += 2
+    if 'college' in ed_found_keywords:
+        points4 += 1
+    if 'major' in ed_found_keywords:
+        points4 += 1
+    if 'minor' in ed_found_keywords:
+        points4 += 1
+    if 'trade school' in ed_found_keywords:
+        points4 += 0.5
+    if 'major' in ed_found_keywords:
+        points4 += 0.25
+rank_ed4()
+ 
+def find_experience_keywords4(text, keywords):
+    found_exp_keywords4 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_exp_keywords.append(keyword)
+    return found_exp_keywords3
+ 
+ # keywords to find for experience
+exp_keywords_to_search4 = ['assisted', 'coordinated', 'intern', 'tutor','teacher', 'leadership']
+exp_found_keywords4 = find_experience_keywords(pdf_text, exp_keywords_to_search)
+
+def rank_exp4():
+    if 'intern' in exp_found_keywords:
+        global points4
+        points4 += 3
+    if 'coordinated' in exp_found_keywords:
+        points4 += 2
+    if 'assisted' in exp_found_keywords:
+        points4 += 1
+    if 'tutor' in exp_found_keywords:
+        points4 += 2
+    if 'leadership' in exp_found_keywords:
+        points4 += 1
+rank_exp4()
+ 
+if exp_found_keywords3:
+    print("Experience keywords found:", exp_found_keywords3)
+else:
+    print("No keywords found.")
+
+def find_skills_keywords4(text, keywords):
+    found_skill_keywords4 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_skill_keywords.append(keyword)
+    return found_skill_keywords
+
+skills_keywords_to_search4 = ['python', 'java', 'c++', 'javascript', 'github', 'html', 'css', 'SQL', 'NoSQL', 'SDLC', 'Scrum', 'Kanban', 'AWS', 'Azure', 'iOS', 'Android', 'Linux', 'Windows']
+skills_found_keywords4 = find_skills_keywords(pdf_text, skills_keywords_to_search)
+
+def find_skills4():
+    if 'python' in skills_found_keywords2:
+        global points4
+        points4 += 1
+    if 'java' in skills_found_keywords2:
+        points4 += 1
+    if 'c++' in skills_found_keywords2:
+        points4 += 1
+    if 'javascript' in skills_found_keywords2:
+        points4 += 1
+    if 'html' in skills_found_keywords2:
+        points4 += 1
+    if 'css' in skills_found_keywords2:
+        points4 += 1
+    if 'SQL' in skills_found_keywords2:
+        points4 += 1
+    if 'Scrum' in skills_found_keywords2:
+        points4 += 1
+    if 'KanBan' in skills_found_keywords2:
+        points4 += 1
+    if 'AWS' in skills_found_keywords2:
+        points4 += 1
+    if 'Azure' in skills_found_keywords2:
+        points4 += 1
+    if 'iOS' in skills_found_keywords2:
+        points4 += 1
+    if 'Android' in skills_found_keywords2:
+        points4 += 1
+    if 'Linux' in skills_found_keywords2:
+        points4 += 1
+    if 'Win' in skills_found_keywords2:
+        points4 += 1
+find_skills4()
+
+if skills_found_keywords4:
+    print("Skills found:", skills_found_keywords3)
+else:
+    print("No keywords found.")
+
+ 
+ # prints points Fuck you vincentFuck you vincentFuck you vincentFuck you vincent
+print(f'Points for Resume 4: {points4}')
+
+points5 = 0 
+
+with open('resume5.pdf', 'rb') as fin:
+    extract_text_to_fp(fin, output_string)
+print(output_string.getvalue().strip())
+ 
+pdf_text = extract_text('resume5.pdf')
+pdf_path = extract_text('resume5.pdf')
+
+def extract_text_from_pdf5(pdf_path):
+    return extract_text(pdf_path)
+
+# finds keywords
+def find_ed_keywords5(text, keywords):
+    found_ed_keywords5 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_ed_keywords.append(keyword)
+    return found_ed_keywords
+
+# keywords to find
+ed_keywords_to_search5 = ['university', 'degree', 'college', 'major', 'minor', 'trade school', 'high school']
+ed_found_keywords5 = find_ed_keywords(pdf_text, ed_keywords_to_search)
+
+ # message to say if keywords are found
+if ed_found_keywords5:
+    print("Keywords found:", ed_found_keywords3)
+else:
+    print("No keywords found.")
+ 
+ # rank keywords related to education 
+def rank_ed5():
+    if 'university' in ed_found_keywords:
+        global points5
+        points5 += 2
+    if 'degree' in ed_found_keywords:
+        points5 += 2
+    if 'college' in ed_found_keywords:
+        points5 += 1
+    if 'major' in ed_found_keywords:
+        points5 += 1
+    if 'minor' in ed_found_keywords:
+        points5 += 1
+    if 'trade school' in ed_found_keywords:
+        points5 += 0.5
+    if 'major' in ed_found_keywords:
+        points5 += 0.25
+rank_ed5()
+ 
+def find_experience_keywords5(text, keywords):
+    found_exp_keywords5 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_exp_keywords.append(keyword)
+    return found_exp_keywords5
+ 
+ # keywords to find for experience
+exp_keywords_to_search5 = ['assisted', 'coordinated', 'intern', 'tutor','teacher', 'leadership']
+exp_found_keywords5 = find_experience_keywords(pdf_text, exp_keywords_to_search)
+
+def rank_exp5():
+    if 'intern' in exp_found_keywords:
+        global points5
+        points5 += 3
+    if 'coordinated' in exp_found_keywords:
+        points5 += 2
+    if 'assisted' in exp_found_keywords:
+        points5 += 1
+    if 'tutor' in exp_found_keywords:
+        points5 += 2
+    if 'leadership' in exp_found_keywords:
+        points5 += 1
+rank_exp5()
+ 
+if exp_found_keywords5:
+    print("Experience keywords found:", exp_found_keywords3)
+else:
+    print("No keywords found.")
+
+def find_skills_keywords5(text, keywords):
+    found_skill_keywords5 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_skill_keywords.append(keyword)
+    return found_skill_keywords
+
+skills_keywords_to_search5 = ['python', 'java', 'c++', 'javascript', 'github', 'html', 'css', 'SQL', 'NoSQL', 'SDLC', 'Scrum', 'Kanban', 'AWS', 'Azure', 'iOS', 'Android', 'Linux', 'Windows']
+skills_found_keywords5 = find_skills_keywords(pdf_text, skills_keywords_to_search)
+
+def find_skills5():
+    if 'python' in skills_found_keywords2:
+        global points5
+        points5 += 1
+    if 'java' in skills_found_keywords2:
+        points5 += 1
+    if 'c++' in skills_found_keywords2:
+        points5 += 1
+    if 'javascript' in skills_found_keywords2:
+        points5 += 1
+    if 'html' in skills_found_keywords2:
+        points5 += 1
+    if 'css' in skills_found_keywords2:
+        points5 += 1
+    if 'SQL' in skills_found_keywords2:
+        points5 += 1
+    if 'Scrum' in skills_found_keywords2:
+        points5 += 1
+    if 'KanBan' in skills_found_keywords2:
+        points5 += 1
+    if 'AWS' in skills_found_keywords2:
+        points5 += 1
+    if 'Azure' in skills_found_keywords2:
+        points5 += 1
+    if 'iOS' in skills_found_keywords2:
+        points5 += 1
+    if 'Android' in skills_found_keywords2:
+        points5 += 1
+    if 'Linux' in skills_found_keywords2:
+        points5 += 1
+    if 'Win' in skills_found_keywords2:
+        points5 += 1
+find_skills5()
+
+if skills_found_keywords5:
+    print("Skills found:", skills_found_keywords5)
+else:
+    print("No keywords found.")
+
+ 
+ # prints points Fuck you vincentFuck you vincentFuck you vincentFuck you vincent
+print(f'Points for Resume 5: {points5}')
+
+points6 = 0 
+
+with open('resume6.pdf', 'rb') as fin:
+    extract_text_to_fp(fin, output_string)
+print(output_string.getvalue().strip())
+ 
+pdf_text = extract_text('resume6.pdf')
+pdf_path = extract_text('resume6.pdf')
+
+def extract_text_from_pdf6(pdf_path):
+    return extract_text(pdf_path)
+
+# finds keywords
+def find_ed_keywords6(text, keywords):
+    found_ed_keywords6 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_ed_keywords.append(keyword)
+    return found_ed_keywords
+
+# keywords to find
+ed_keywords_to_search6 = ['university', 'degree', 'college', 'major', 'minor', 'trade school', 'high school']
+ed_found_keywords6 = find_ed_keywords(pdf_text, ed_keywords_to_search)
+
+ # message to say if keywords are found
+if ed_found_keywords6:
+    print("Keywords found:", ed_found_keywords5)
+else:
+    print("No keywords found.")
+ 
+ # rank keywords related to education 
+def rank_ed6():
+    if 'university' in ed_found_keywords:
+        global points6
+        points6 += 2
+    if 'degree' in ed_found_keywords:
+        points6 += 2
+    if 'college' in ed_found_keywords:
+        points6 += 1
+    if 'major' in ed_found_keywords:
+        points6 += 1
+    if 'minor' in ed_found_keywords:
+        points6 += 1
+    if 'trade school' in ed_found_keywords:
+        points6 += 0.5
+    if 'major' in ed_found_keywords:
+        points6 += 0.25
+rank_ed6()
+ 
+def find_experience_keywords6(text, keywords):
+    found_exp_keywords6 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_exp_keywords.append(keyword)
+    return found_exp_keywords6
+ 
+ # keywords to find for experience
+exp_keywords_to_search6 = ['assisted', 'coordinated', 'intern', 'tutor','teacher', 'leadership']
+exp_found_keywords6 = find_experience_keywords(pdf_text, exp_keywords_to_search)
+
+def rank_exp6():
+    if 'intern' in exp_found_keywords:
+        global points6
+        points6 += 3
+    if 'coordinated' in exp_found_keywords:
+        points6 += 2
+    if 'assisted' in exp_found_keywords:
+        points6 += 1
+    if 'tutor' in exp_found_keywords:
+        points6 += 2
+    if 'leadership' in exp_found_keywords:
+        points6 += 1
+rank_exp6()
+ 
+if exp_found_keywords6:
+    print("Experience keywords found:", exp_found_keywords6)
+else:
+    print("No keywords found.")
+
+def find_skills_keywords6(text, keywords):
+    found_skill_keywords6 = []
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            found_skill_keywords.append(keyword)
+    return found_skill_keywords
+
+skills_keywords_to_search6 = ['python', 'java', 'c++', 'javascript', 'github', 'html', 'css', 'SQL', 'NoSQL', 'SDLC', 'Scrum', 'Kanban', 'AWS', 'Azure', 'iOS', 'Android', 'Linux', 'Windows']
+skills_found_keywords6 = find_skills_keywords(pdf_text, skills_keywords_to_search)
+
+def find_skills6():
+    if 'python' in skills_found_keywords6:
+        global points6
+        points6 += 1
+    if 'java' in skills_found_keywords6:
+        points6 += 1
+    if 'c++' in skills_found_keywords6:
+        points6 += 1
+    if 'javascript' in skills_found_keywords6:
+        points6 += 1
+    if 'html' in skills_found_keywords2:
+        points6 += 1
+    if 'css' in skills_found_keywords2:
+        points6 += 1
+    if 'SQL' in skills_found_keywords2:
+        points6 += 1
+    if 'Scrum' in skills_found_keywords2:
+        points6 += 1
+    if 'KanBan' in skills_found_keywords2:
+        points6 += 1
+    if 'AWS' in skills_found_keywords2:
+        points6 += 1
+    if 'Azure' in skills_found_keywords2:
+        points6 += 1
+    if 'iOS' in skills_found_keywords2:
+        points6 += 1
+    if 'Android' in skills_found_keywords2:
+        points6 += 1
+    if 'Linux' in skills_found_keywords2:
+        points6 += 1
+    if 'Win' in skills_found_keywords2:
+        points6 += 1
+find_skills6()
+
+if skills_found_keywords6:
+    print("Skills found:", skills_found_keywords6)
+else:
+    print("No keywords found.")
+
+ 
+ # prints points Fuck you vincentFuck you vincentFuck you vincentFuck you vincent
+print(f'Points for Resume 6: {points6}')
+
+print(f'Points for eachh Resume: 1: {points}, 2: {points2}, 3: {points3}, 4: {points4}, 5: {points5}, 6: {points6}')
